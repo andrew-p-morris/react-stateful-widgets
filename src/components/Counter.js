dd74c037375466c07b85fd9c73957177db16db70
MacOS <macos@Drews-MacBook-Pro.local>
2021-03-24 19:42:48 -0700

    Counter.js

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -46,32 +46,47 @@ STEP 6:
   This click handler needs to use 'setCount' to set the 'count' to be zero again.
 */
 
-import React from 'react'; /* STEP 0 */
+import React, {useState} from 'react'; /* STEP 0 */
 
 export default function Counter() {
-  /* STEP 1 */
+  const [count, setCount] = useState(0)
 
-  const increment = () => {
-    /* STEP 4 */
+  const increment = () => {setCount( count + 1)
+    
+    
   };
-  const decrement = () => {
-    /* STEP 5 */
+  const decrement = () => {setCount (count - 1)
+  
   };
-  const reset = () => {
-    /* STEP 6 */
+  const reset = () => {setCount (0)
+    
   };
 
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
     color: 'royalblue', /* STEP 2 */
+    
   };
+
+  let stringVariable = 'even'
+
+  if( count %2 == 0){
+    stringVariable = 'even';
+    style.color = 'royalblue'
+  }
+  else{
+   stringVariable = 'odd'; 
+   style.color = 'crimson' 
+  }
+
+ 
 
   return (
     <div className='widget-counter container'>
       <h2>Counter</h2>
       <div id='count' style={style}>
-        Number 0 is even {/* STEP 3 */}
+        Number {count} is {stringVariable}
       </div>
       <div>
         <button id='increment' onClick={increment}>Increment</button>
